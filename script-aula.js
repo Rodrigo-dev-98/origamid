5408f1c1243c345ac0791fdfae1aff46a801126b
Rodrigo Oliveira <rodrigo42538@gmail.com>
2022-06-26 21:38:51 -0300

    Add files via upload

---
+++ script-aula.js
@@ -0,0 +1,27 @@
+var nome = 'Rodrigo';
+var idade = 24;
+var comidaFavorita;
+
+console.log(typeof nome,typeof idade,typeof comidaFavorita);
+
+var nome = 'Rodrigo';
+var sobrenome = 'Costa';
+var nomeCompleto = nome +' '+ sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'O Flamengo fez mais de ' + gols + ' gols';
+console.log(frase);
+
+var ano = 2022;
+var mes = 6;
+console.log(ano + mes);
+
+var melhor = 'teste'
+var frase1 = "Esse é o \"melhor\" jogo"
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `O Flamengo fez mais de ${gols * 2} gols`;
+console.log(frase2);
---
+++ script.js
@@ -0,0 +1,24 @@
+var nome = 'Rodrigo';
+console.log(typeof nome)
+
+var ano = '2018';
+
+var idade = 24;
+var anos = `Eu tenho ${idade} de idade`;
+console.log(anos);
+
+var sobrenome = 'costa'
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto);
+
+var ingles = 'it\'s\ time';
+console.log(ingles);
+
+
+
+
+
+
+
+
+
